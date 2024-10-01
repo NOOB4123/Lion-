6d7b1ba2ad0d4b139c00d97f42e58631dae2a1c9
NOOB4123 <110089674+NOOB4123@users.noreply.github.com>
2024-10-01 17:35:47 +0500

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Starting score
-let score = 0;
+// Starting coin count
+let coins = 0;
 
-// Function to increase score when coin is tapped
-function tapCoin() {
-    score++; // Increment score by 1
-    document.getElementById('score').textContent = score; // Update score display
+// Function to increase coins when lion is tapped
+function mineCoins() {
+    coins += 1; // Increment coins by 1
+    document.getElementById('coinCount').textContent = coins; // Update coin count display
 }
